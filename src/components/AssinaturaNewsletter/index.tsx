629dfa07bd7f05410349941a4894523dde0c4f2c
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-03-31 22:14:47 -0300

    refactor(assinaturanewsletter): create AssinaturaNewsletter component and start adding styles

---
+++ src/components/AssinaturaNewsletter/index.tsx
@@ -0,0 +1,9 @@
+export function AssinaturaNewsletter() {
+	return (
+		<>
+			<label htmlFor="newsletter-input">Insira seu e-mail</label>
+			<input id="newsletter-input" type="email" placeholder="Insira seu e-mail" />
+			<button>Assinar newsletter</button>
+		</>
+	);
+}
