0c71a88592df9909c41b698c93763028c970019e
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-03-31 22:05:35 -0300

    refactor(home and main): rename App.tsx and move it to pages/Home. Import global.css in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import HomePage from './pages/Home';
+
+import './styles/global.css';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<HomePage />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Menu } from '../../components/Menu';
+import { AssinaturaNewsletter } from '../../components/AssinaturaNewsletter';
+
+export default function HomePage() {
+	return (
+		<>
+			<Menu />
+			<AssinaturaNewsletter />
+		</>
+	);
+}
