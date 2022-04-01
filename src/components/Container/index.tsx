8aa93c9cecf9a5e45190ca855731a9a6cea3d26c
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-03-31 22:12:22 -0300

    refactor(container): create Container component and start adding styles

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,6 @@
+import { Children } from 'react';
+import { StyledContainer } from './styled';
+
+export function Container({ children }) {
+	return <StyledContainer>{children}</StyledContainer>;
+}
---
+++ src/components/Container/styled.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.div`
+	width: 100%;
+	max-width: 1203px;
+	margin: 0 auto;
+
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
