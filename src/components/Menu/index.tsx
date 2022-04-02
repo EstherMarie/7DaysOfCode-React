c5658b0941a2ddf303f500051a9c27094d018c0a
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-04-02 20:01:01 -0300

    refactor(menu): change style of component
    Modify alignment of nav elements, add margin between list items and hide first list items in smaller
    screens.
    
    add h1 and change breakpoint

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,21 +7,23 @@ export function Menu() {
 	return (
 		<StyledMenu>
 			<Container>
-				<img src={LogoCasaVerde} alt="CasaVerde" />
+				<h1>
+					<img src={LogoCasaVerde} alt="CasaVerde" />
+				</h1>
 
 				<nav>
 					<StyledList>
 						<li>
-							<a href="#como-fazer">Como fazer</a>
+							<a href="#como-fazer">Como fazer</a>/
 						</li>
 						<li>
-							<a href="#ofertas">Ofertas</a>
+							<a href="#ofertas">Ofertas</a>/
 						</li>
 						<li>
-							<a href="#depoimentos">Depoimentos</a>
+							<a href="#depoimentos">Depoimentos</a>/
 						</li>
 						<li>
-							<a href="#videos">Vídeos</a>
+							<a href="#videos">Vídeos</a>/
 						</li>
 						<li>
 							<a href="#carrinho">Meu carrinho</a>
--- src/components/Menu/styled.tsx
+++ src/components/Menu/styled.tsx
@@ -3,15 +3,33 @@ import styled from 'styled-components';
 export const StyledMenu = styled.header`
 	display: flex;
 	justify-content: space-between;
+
+	margin-top: 10.16px;
+
+	nav {
+		height: 100%;
+		display: flex;
+		align-items: flex-end;
+	}
 `;
 
 export const StyledList = styled.ul`
 	display: flex;
 	justify-content: space-between;
 
-	li:nth-child(-n + 4) {
-		&::after {
-			content: ' /';
+	li a {
+		margin: 0 1rem;
+	}
+
+	li:last-child {
+		font-weight: 600;
+		margin-right: 0;
+		margin-left: 38px;
+	}
+
+	@media (max-width: 992px) {
+		li:nth-child(-n + 4) {
+			display: none;
 		}
 	}
 `;
