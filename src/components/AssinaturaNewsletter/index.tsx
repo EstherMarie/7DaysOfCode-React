435b79f87cef2d57fa03ec9920583ed5a6828509
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-04-02 22:48:39 -0300

    refactor(assinaturanewsletter): add relevant improvements to the component
    Finished working on basic layout of the component.

--- src/components/AssinaturaNewsletter/index.tsx
+++ src/components/AssinaturaNewsletter/index.tsx
@@ -1,9 +1,27 @@
+import { Container } from '../Container';
+import { StyledNewsletter } from './styled';
+
 export function AssinaturaNewsletter() {
 	return (
-		<>
-			<label htmlFor="newsletter-input">Insira seu e-mail</label>
-			<input id="newsletter-input" type="email" placeholder="Insira seu e-mail" />
-			<button>Assinar newsletter</button>
-		</>
+		<Container>
+			<StyledNewsletter>
+				<div className="text-area">
+					<h2>
+						<div>Sua casa com </div>as melhores plantas
+					</h2>
+					<p>
+						Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu
+						dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.
+					</p>
+				</div>
+				<form action="">
+					<label htmlFor="newsletter-input" className="sr-only">
+						Insira seu e-mail
+					</label>
+					<input id="newsletter-input" type="email" placeholder="Insira seu e-mail" />
+					<button>Assinar newsletter</button>
+				</form>
+			</StyledNewsletter>
+		</Container>
 	);
 }
