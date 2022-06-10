1100678827e33109047992e30051074ba59510c3
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-06-09 23:34:58 -0300

    refactor(ofertas): add responsive

--- src/components/ConheçaNossasOfertas/ConheçaNossasOfertas.styled.ts
+++ src/components/ConheçaNossasOfertas/ConheçaNossasOfertas.styled.ts
@@ -1,36 +1,49 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StyledConheçaNossasOfertas = styled.section`
-  width: 1200px;
-  margin: auto;
-  margin-bottom: 153px;
+	max-width: 1200px;
+	margin: auto;
+	margin-bottom: 153px;
 
-  h2 {
-    font-family: var(--font-title);
-    font-size: 82px;
-    font-weight: 900;
-    line-height: 94px;
+	h2 {
+		font-family: var(--font-title);
+		font-size: 82px;
+		font-weight: 900;
+		line-height: 94px;
 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 12px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 12px;
 
-    margin-bottom: 5px;
+		margin-bottom: 5px;
 
-    span {
-      font-family: var(--font-body);
-      font-size: 22px;
-      font-weight: 400;
-      line-height: 27px;
-      color: var(--black);
-      opacity: 0.5;
-    }
-  }
+		span {
+			font-family: var(--font-body);
+			font-size: 22px;
+			font-weight: 400;
+			line-height: 27px;
+			color: var(--black);
+			opacity: 0.5;
+		}
+	}
 
-  ul {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 30px;
-  }
+	ul {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-items: center;
+		gap: 30px;
+
+		@media screen and (max-width: 1500px) {
+			padding-inline: 15px;
+		}
+	}
+
+	@media screen and (max-width: 600px) {
+		h2 {
+			font-size: 80px;
+		}
+	}
 `;
--- src/components/ConheçaNossasOfertas/Oferta/Oferta.styled.ts
+++ src/components/ConheçaNossasOfertas/Oferta/Oferta.styled.ts
@@ -1,64 +1,77 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StyledOferta = styled.li`
-  width: 380px;
-  height: 200px;
+	max-width: 380px;
+	width: 100%;
+	height: 200px;
 
-  background-color: var(--white);
+	background-color: var(--white);
 
-  display: grid;
-  grid-template-columns: 204px 1fr;
+	box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.06);
 
-  box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.06);
+	display: grid;
+	grid-template-columns: minmax(30%, 204px) 1fr;
 
-  .img-area {
-    max-width: 204px;
-    height: 100%;
-    overflow: hidden;
+	@media screen and (max-width: 400px) {
+		grid-template-columns: 1fr 150px;
+	}
+	.img-area {
+		max-width: 204px;
+		height: 100%;
+		overflow: hidden;
 
-    img {
-      width: 100%;
-      height: 100%;
-    }
-  }
+		img {
+			width: 100%;
+			height: 100%;
+		}
+	}
 
-  .text-area {
-    padding-top: 27px;
-    padding-bottom: 27px;
+	.text-area {
+		padding-top: 27px;
+		padding-bottom: 27px;
+		padding-right: 5px;
 
-    h3 {
-      font-family: var(--font-title);
-      font-weight: 900;
-      font-size: 32px;
-      line-height: 37px;
-    }
+		@media screen and (max-width: 400px) {
+			padding-bottom: 10px;
+		}
 
-    .preco {
-      margin-top: 8px;
-      margin-bottom: 24px;
+		h3 {
+			font-family: var(--font-title);
+			font-weight: 900;
+			font-size: 32px;
+			line-height: 37px;
 
-      font-family: var(--font-body);
-      font-style: normal;
-      font-weight: 400;
-      font-size: 16px;
-      line-height: 20px;
+			@media screen and (max-width: 400px) {
+				font-size: 30px;
+			}
+		}
 
-      opacity: 0.5;
-    }
+		.preco {
+			margin-top: 8px;
+			margin-bottom: 24px;
 
-    .link-comprar {
-      font-family: var(--font-body);
-      font-style: normal;
-      font-weight: 400;
-      font-size: 16px;
-      line-height: 20px;
+			font-family: var(--font-body);
+			font-style: normal;
+			font-weight: 400;
+			font-size: 16px;
+			line-height: 20px;
 
-      color: var(--main-color);
+			opacity: 0.5;
+		}
 
-      &::after {
-        content: url("/assets/svg/seta.svg");
-        padding-left: 25px;
-      }
-    }
-  }
+		.link-comprar {
+			font-family: var(--font-body);
+			font-style: normal;
+			font-weight: 400;
+			font-size: 16px;
+			line-height: 20px;
+
+			color: var(--main-color);
+
+			&::after {
+				content: url('/assets/svg/seta.svg');
+				padding-left: 25px;
+			}
+		}
+	}
 `;
