6a49317ebea1534f2fc8b74fd1edc143942b4654
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-06-10 00:19:37 -0300

    refactor(ComoConseguir): add responsive

--- src/components/ComoConseguir/ComoConseguir.styled.ts
+++ src/components/ComoConseguir/ComoConseguir.styled.ts
@@ -1,74 +1,101 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StyledComoConseguir = styled.section`
-  width: 995px;
-  height: 440px;
+	max-width: 995px;
+	width: 100%;
+	height: 440px;
 
-  margin: 228px auto 44px;
-  background-color: var(--white);
-  box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.06);
+	margin: 228px auto 44px;
+	background-color: var(--white);
+	box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.06);
 
-  display: flex;
+	display: flex;
 
-  .img-area {
-    width: 585px;
-    height: 100%;
-    background-image: url("/assets/images/image-3.png");
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
+	.img-area {
+		width: 585px;
+		height: 100%;
+		background-image: url('/assets/images/image-3.png');
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
 
-  .text-area {
-    padding: 58px 81px 43px 32px;
+	.text-area {
+		padding: 58px 81px 43px 32px;
 
-    h2 {
-      font-family: var(--font-title);
-      font-size: 42px;
-      font-weight: 900;
-      line-height: 48px;
+		h2 {
+			font-family: var(--font-title);
+			font-size: 42px;
+			font-weight: 900;
+			line-height: 48px;
 
-      span {
-        font-family: var(--font-body);
-        font-size: 22px;
-        font-weight: 400;
-        line-height: 26.82px;
-        color: var(--black);
-        opacity: 0.5;
+			span {
+				font-family: var(--font-body);
+				font-size: 22px;
+				font-weight: 400;
+				line-height: 26.82px;
+				color: var(--black);
+				opacity: 0.5;
 
-        display: block;
-      }
-    }
+				display: block;
+			}
+		}
 
-    ol {
-      margin-top: 32px;
+		ol {
+			margin-top: 32px;
 
-      display: flex;
-      flex-direction: column;
-      gap: 32px;
+			display: flex;
+			flex-direction: column;
+			gap: 32px;
 
-      li {
-        font-family: var(--font-body);
-        font-weight: 400;
-        font-size: 22px;
-        line-height: 26px;
-        opacity: 0.5;
+			li {
+				font-family: var(--font-body);
+				font-weight: 400;
+				font-size: 22px;
+				line-height: 26px;
+				opacity: 0.5;
 
-        display: flex;
-        align-items: center;
+				display: flex;
+				align-items: center;
 
-        height: 52px;
+				height: 52px;
 
-        &::before {
-          content: "";
-          background-color: var(--main-color);
-          width: 52px;
-          height: 52px;
-          border-radius: 50%;
-          /* opacity: 1; */
+				&::before {
+					content: '';
+					background-color: var(--main-color);
+					/* width: 52px;
+					height: 52px; */
+					border-radius: 50%;
+					padding: 26px;
+					/* opacity: 1; */
 
-          margin-right: 16px;
-        }
-      }
-    }
-  }
+					margin-right: 16px;
+
+					@media screen and (max-width: 400px) {
+						padding: 20px;
+					}
+				}
+			}
+		}
+
+		@media screen and (max-width: 1540px) {
+			padding-right: 65px;
+		}
+
+		@media screen and (max-width: 600px) {
+			padding-inline: 20px;
+
+			h2 {
+				font-size: 38px;
+			}
+		}
+	}
+
+	@media screen and (max-width: 1000px) {
+		flex-direction: column;
+		min-height: 860px;
+
+		.img-area {
+			width: 100%;
+		}
+	}
 `;
--- src/components/ComoConseguir/index.tsx
+++ src/components/ComoConseguir/index.tsx
@@ -1,19 +1,22 @@
-import { StyledComoConseguir } from "./ComoConseguir.styled";
+import { StyledComoConseguir } from './ComoConseguir.styled';
+import { Container } from '../Container';
 
 export function ComoConseguir() {
-  return (
-    <StyledComoConseguir id="como-fazer">
-      <div className="img-area"></div>
-      <div className="text-area">
-        <h2>
-          <span>Como conseguir </span>minha planta
-        </h2>
-        <ol>
-          <li>Escolha suas plantas</li>
-          <li>Faça seu pedido</li>
-          <li>Aguarde na sua casa</li>
-        </ol>
-      </div>
-    </StyledComoConseguir>
-  );
+	return (
+		<Container>
+			<StyledComoConseguir id="como-fazer">
+				<div className="img-area"></div>
+				<div className="text-area">
+					<h2>
+						<span>Como conseguir </span>minha planta
+					</h2>
+					<ol>
+						<li>Escolha suas plantas</li>
+						<li>Faça seu pedido</li>
+						<li>Aguarde na sua casa</li>
+					</ol>
+				</div>
+			</StyledComoConseguir>
+		</Container>
+	);
 }
